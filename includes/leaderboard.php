5ded39ccd5a88f133454d5e5b25aa5f4e7f540ad
s7amen <93478622+s7amen@users.noreply.github.com>
2025-06-04 09:13:42 +0300

    добавяне на центрирани редове и нова колона Място  leaderboard.php

--- includes/leaderboard.php
+++ includes/leaderboard.php
@@ -57,25 +57,28 @@ function bsp_leaderboard_shortcode($atts) {
     echo '<h3 class="text-xl font-semibold mb-4 text-center">Класиране</h3>';
     echo '<table class="min-w-full border border-gray-300 text-sm">';
     echo '<thead><tr>
+        <th class="px-3 py-2 border text-center">Място</th>
         <th class="px-3 py-2 border">Състезател</th>
-        <th class="px-3 py-2 border text-right">Точки</th>
+        <th class="px-3 py-2 border text-center">Точки</th>
         <th class="px-3 py-2 border text-center">Топове</th>
         <th class="px-3 py-2 border text-center">Зони</th>
         <th class="px-3 py-2 border text-center">Опити (топ)</th>
         <th class="px-3 py-2 border text-center">Опити (зона)</th>
     </tr></thead><tbody>';
 
+    $place = 1;
     foreach ($users as $u) {
         echo '<tr>
+            <td class="px-3 py-2 border text-center">' . $place . '</td>
             <td class="px-3 py-2 border">' . esc_html($u['username']) . '</td>
-            <td class="px-3 py-2 border text-right">' . round($u['score'], 1) . '</td>
-            <td class="px-3 py-2 border text-center">' . $u['tops'] . '</td>
-            <td class="px-3 py-2 border text-center">' . $u['zones'] . '</td>
-            <td class="px-3 py-2 border text-center">' . $u['top_attempts'] . '</td>
-            <td class="px-3 py-2 border text-center">' . $u['zone_attempts'] . '</td>
+            <td class="px-3 py-2 border text-center">' . esc_html(round($u['score'], 1)) . '</td>
+            <td class="px-3 py-2 border text-center">' . esc_html($u['tops']) . '</td>
+            <td class="px-3 py-2 border text-center">' . esc_html($u['zones']) . '</td>
+            <td class="px-3 py-2 border text-center">' . esc_html($u['top_attempts']) . '</td>
+            <td class="px-3 py-2 border text-center">' . esc_html($u['zone_attempts']) . '</td>
         </tr>';
+        $place++;
     }
-
     echo '</tbody></table></div>';
     return ob_get_clean();
 }
